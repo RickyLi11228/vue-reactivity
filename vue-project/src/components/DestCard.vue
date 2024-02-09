d1cca7b78378bec57974fa77d4454d77d965882d
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2024-02-09 10:15:08 -0500

    Ricky Clicker

--- vue-project/src/components/DestCard.vue
+++ vue-project/src/components/DestCard.vue
@@ -1,7 +1,9 @@
 <template>
   <h3>{{ clicked }}</h3>
   <button @click="increment">Click Me</button>
+  <h3>{{ upgrade1 }}</h3>
   <button @click="upgrader1">Click Me</button>
+  <h3>Cost: {{ upgrade1_cost }}</h3>
 </template>
   
   <script setup>
@@ -11,20 +13,26 @@
   });
   
   //clicker logic
-  let clicked = ref(0);
+  let upgrade1_cost = 1
+  const clicked = ref(0);
   let clicklyclicker = 1
   function increment() {
     clicked.value = clicked.value + clicklyclicker;
     console.log(clicked.value)
   }
-  let upgrade1 = ref(0)
+  let upgrade1 = ref(1)
   function upgrader1() {
+    if (upgrade1_cost <= clicked.value){
     upgrade1.value = upgrade1.value + 1
     console.log(upgrade1.value)
-  }
-let clicklyclicker = clicklyclicker * upgrade1.value
-  
+    clicklyclicker = 1 * upgrade1.value
+    clicked.value = clicked.value - upgrade1_cost
+    upgrade1_cost = upgrade1_cost * 2
+    }
+    
 
+  }
+  
   </script>
   
   <style scoped>
